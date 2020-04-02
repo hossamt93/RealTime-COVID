ee0e2b4a3d1257432d8537059c4f566d8885df32
Hossam <msdn41@getgroup.com>
2020-04-02 07:05:44 +0200

    fix realtime render update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,50 +29,58 @@ export class AppComponent implements OnInit {
   COVIDData: ICOVID[];
   countriesLabels: string[] = [];
   currenCountryIndex = 0;
-  selectedCountries:ICOVID[]=[];
+  selectedCountries: ICOVID[] = [];
 
-  pieChartLabels:string[]=['Cases', 'Today Cases', 'Deaths', 'Today Deaths', 'Recovered', 'Active', 'Critical', 'Cases / Million', 'Deaths / Million'];
-  barChartLabels:string[]=['Cases', 'Today Cases', 'Deaths', 'Today Deaths', 'Recovered', 'Active'];
+  pieChartLabels: string[] = ['Cases', 'Today Cases', 'Deaths', 'Today Deaths', 'Recovered', 'Active', 'Critical', 'Cases per one Million', 'Deaths per one Million'];
+  barChartLabels: string[] = ['Cases', 'Today Cases', 'Deaths', 'Today Deaths', 'Recovered', 'Active'];
   barDataSets: IBarChartItem[] = [];
   pieDataSets: number[] = Array(9).fill(0);
   barChart: any;
   pieChart: any;
-  // sortArray:string[]=['Country','Cases'];
-  // displayItems:number[]=[5,10,15];
+ 
 
   constructor(private COVIDService: COVIDService) {
 
   }
   ngOnInit(): void {
-    //this.getCOVIDData();
-    setInterval(()=>this.getCOVIDData(), 18000);
+    this.getCOVIDData(false);
+    setInterval(() => this.getCOVIDData(false), 180000);
   }
 
-  private getCOVIDData() {
+  private getCOVIDData(destroy: boolean) {
     this.COVIDService.getCOVIDData().subscribe((result: ICOVID[]) => {
       this.COVIDData = result;
       this.COVIDData.sort((a, b) => b.cases - a.cases)
       this.dataSource = new MatTableDataSource(this.COVIDData);
       this.dataSource.paginator = this.paginator;
       this.dataSource.sort = this.sort;
-      this.getNextCountries();
+      this.getNextCountries(destroy,false);
     })
   }
 
-  rendercharts() {
-    if (this.barChart)
-      this.barChart.destroy();
+  rendercharts(destroy: boolean) {
+    let barCanvas: any;
+    let pieCanvas: any;
+debugger
+    if(!barCanvas)
+      barCanvas = document.getElementById('barChart');
+    if(!pieCanvas)
+      pieCanvas = document.getElementById('pieChart');
 
-    if (this.pieChart)
-      this.pieChart.destroy();
+    if (destroy || this.barChart ==undefined || this.pieChart==undefined) {
+      if (this.barChart)
+        this.barChart.destroy();
 
-    let barCanvas: any = document.querySelector('.bar-chart');
-    barCanvas.innerHTML = ` <canvas id="barChart" style="width: 100%;height: 500px;margin-bottom: 20px; display:block;"></canvas>`;
-    barCanvas = document.getElementById('barChart');
+      if (this.pieChart)
+        this.pieChart.destroy();
 
-    let pieCanvas: any = document.querySelector('.pie-chart');
-    pieCanvas.innerHTML = ` <canvas id="pieChart" style="width: 100%;height: 500px;margin-bottom: 20px; padding-left: 40px; display:block;"></canvas>`;
-    pieCanvas = document.getElementById('pieChart');
+      barCanvas = document.querySelector('.bar-chart');
+      barCanvas.innerHTML = ` <canvas id="barChart" style="width: 100%;height: 500px;margin-bottom: 20px; display:block;"></canvas>`;
+      barCanvas = document.getElementById('barChart');
+
+      pieCanvas = document.querySelector('.pie-chart');
+      pieCanvas.innerHTML = ` <canvas id="pieChart" style="width: 100%;height: 500px;margin-bottom: 20px; padding-left: 40px; display:block;"></canvas>`;
+      pieCanvas = document.getElementById('pieChart');
 
     let barCtx = barCanvas.getContext('2d');
     let pieCtx = pieCanvas.getContext('2d');
@@ -112,14 +120,21 @@ export class AppComponent implements OnInit {
         }
       }
     });
-
+  }
+    else{
+      // update charts datasets
+      this.pieChart.data.datasets[0].data=this.pieDataSets;
+      this.barChart.data.datasets=this.barDataSets;
+      this.pieChart.update();
+      this.barChart.update();
+    }
   }
 
-  updateChartItems(COVIDCountries:ICOVID[]) {
+  updateChartItems(COVIDCountries: ICOVID[],destroyCharts:boolean) {
     this.pieDataSets = Array(9).fill(0);
     this.countriesLabels = [];
     this.barDataSets = [];
-    COVIDCountries.forEach(item=> {
+    COVIDCountries.forEach(item => {
       this.countriesLabels.push(item.country);
       this.pieDataSets[0] += item.cases;
       this.pieDataSets[1] += item.todayCases;
@@ -136,15 +151,15 @@ export class AppComponent implements OnInit {
         backgroundColor: this.getRandomchartColor(1)[0],
         borderWidth: 1,
         data: [item.cases,
-          item.todayCases,
-          item.deaths,
-          item.todayDeaths,
-          item.recovered,
-          item.active,
-          item.critical]
+        item.todayCases,
+        item.deaths,
+        item.todayDeaths,
+        item.recovered,
+        item.active,
+        item.critical]
       });
     });
-    this.rendercharts();
+    this.rendercharts(destroyCharts);
   }
 
   getRandomchartColor(count): string[] {
@@ -152,7 +167,7 @@ export class AppComponent implements OnInit {
     for (let i = 0; i < count; i++) {
       const randomColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
       chartColors.push(randomColor);
-      
+
     }
     return chartColors;
   }
@@ -165,8 +180,13 @@ export class AppComponent implements OnInit {
     }
   }
 
-  getNextCountries() {
-    this.selectedCountries=[];
+  getNextCountries(destroyCharts:boolean,isButtonEvent:boolean) {
+
+    //stay on the same index if not click event
+    if(!isButtonEvent && this.currenCountryIndex>=6)
+      this.currenCountryIndex=this.currenCountryIndex - 6;
+
+    this.selectedCountries = [];
     for (let index = this.currenCountryIndex; index < this.COVIDData.length; index++) {
       if (index == this.currenCountryIndex + 6) {
         this.currenCountryIndex = index;
@@ -174,14 +194,14 @@ export class AppComponent implements OnInit {
       }
       this.selectedCountries.push(this.COVIDData[index])
     }
-    this.updateChartItems(this.selectedCountries);
+    this.updateChartItems(this.selectedCountries,destroyCharts);
   }
 
-  onCountryChange(country){
-    let item=this.COVIDData.find(x=>x.country==country);
-    this.selectedCountries=[];
+  onCountryChange(country) {
+    let item = this.COVIDData.find(x => x.country == country);
+    this.selectedCountries = [];
     this.selectedCountries.push(item);
-    this.updateChartItems(this.selectedCountries);
+    this.updateChartItems(this.selectedCountries,true);
   }
 }
 
